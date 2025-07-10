1897a0a4f0fc4ebcc9c2c654d3f884d51d487a13
Your_Name <154525691+choco-melk@users.noreply.github.com>
2025-07-10 15:00:33 +0800

    Created Frontend for Website and Finished Dashboard

---
+++ inventory/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="stylesheets/styles.css">
+</head>
+<body>
+    <aside>
+        <a href="../" class="logo">
+            <figure><img src="../images/aacj-logo.png"/></figure>
+            <hgroup>
+                <p class="logo-top">AACJ</p>
+                <p class="logo-bottom">Cycle Parts</p>
+            </hgroup>
+        </a>  
+        <nav>
+            <a href="../" class="navigation-link">
+                <figure>
+                    <img src="../images/dashboard.png"/>
+                </figure>
+                <p>Dashboard</p>
+            </a>
+            <a href="../inventory/" class="navigation-link current-page">
+                <figure>
+                    <img src="../images/inventory-active.png"/>
+                </figure>
+                <p>Inventory</p>
+            </a>
+            <a href="../create-invoice/" class="navigation-link">
+                <figure>
+                    <img src="../images/create-invoice.png"/>
+                </figure>
+                <p>Create Invoice</p>
+            </a>
+            <a href="../purchase-history/" class="navigation-link">
+                <figure>
+                    <img src="../images/purchase-history.png"/>
+                </figure>
+                <p>Purchase History</p>
+            </a>
+        </nav>
+    </aside>
+</body>
+</html>
+
+
+<!-- 
+    <div class="content-section">
+        <header>
+            <h1>Dashboard</h1>
+            <h3>What to do today?</h3>
+        </header>
+    </div> -->
